1d3178d51e3d66872850e4be786e252220075fe8
Ahmad Budairi <budairi.contact@gmail.com>
2023-04-10 12:14:49 +0700

    buat pemformat waktu

---
+++ resources/js/Components/TimeFormat.vue
@@ -0,0 +1,24 @@
+<template>
+  {{ formatedDate }}
+</template>
+
+<script setup>
+import {computed, ref} from "vue";
+
+const props = defineProps(['time'])
+const formatedDate = computed(() => {
+  const regex = /^\d{2}:\d{2}:\d{2}$/;
+  let formatedDate;
+  if (regex.test(props.time)) {
+    const date = new Date(`2023-04-10 ${props.time}`)
+    formatedDate = date.getHours() + ':' + date.getMinutes()
+  } else {
+    formatedDate = props.time
+  }
+  return formatedDate
+})
+</script>
+
+<style scoped>
+
+</style>
